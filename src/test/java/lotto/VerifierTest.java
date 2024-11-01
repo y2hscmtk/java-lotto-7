788e4607214e07924a7cc607673a488560dc55ba
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-11-01 22:35:29 +0900

    test(VerifierTest): 로또 번호 범위 테스트 작성
    - 번호가 1에서 45사이의 값이 아니라면 예외 발생

--- src/test/java/lotto/VerifierTest.java
+++ src/test/java/lotto/VerifierTest.java
@@ -1,6 +1,10 @@
 package lotto;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class VerifierTest {
@@ -19,6 +23,15 @@ class VerifierTest {
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(()-> new Verifier().validatePurchaseAmount(1001))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 로또_번호가_1에서_45사이의_값이_아니라면_예외가_발생_한다() {
+        List<Integer> checkNumbers = Arrays.asList(-1,0,46);
+        for (Integer checkNumber : checkNumbers) {
+            assertThatThrownBy(()-> new Verifier().lottoNumberRangeCheck(checkNumber))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
     }
 
 }
